18d2ae37388570f1dd7fb3d20509fdce9e4d6f55
manu0616 <manu0616gonzalez@gmail.com>
2026-08-14 21:43:21 -0500

    cambis

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 
 void main() {
     System.out.println("que tal el dia querido amigo");
+    System.out.println("que tal el dia querido amigo");
+    System.out.println("que tal el dia querido amigo");
+
+    System.out.println("que tal el dia querido amigo");
+
+    System.out.println("que tal el dia querido amigo");
+    System.out.println("que tal el dia querido amigo");
+
 }
